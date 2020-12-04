e603ff2977d0bb27bed5bec8e6289ef3d1017992
unpluggedk <unpluggedk@users.noreply.github.com>
2020-12-03 16:11:20 -0800

    add new project

---
+++ NewsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsList
+//
+//  Created by Jason Kim on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
