b9f4b3ef53e10b069f1f0caabe22437854532647
unpluggedk <unpluggedk@users.noreply.github.com>
2020-12-04 16:09:41 -0800

    add initial news list collectionview layout

--- NewsList/Main/ListViewController.swift
+++ NewsList/Main/ListViewController.swift
@@ -36,4 +36,60 @@ class ListViewController: UIViewController {
 
 }
 
+// MARK: - UI
+extension ListViewController {
+    enum Section: Int {
+        case main
+        case listing
+    }
+    
+    func generateLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+           
+            guard let sectionLayoutKind = Section(rawValue: sectionIndex) else { return nil }
+            
+            switch sectionLayoutKind {
+            case .main: return self.generateMainLayout()
+            case .listing: return self.generateListingLayout()
+            }
+        }
+        return layout
+    }
+    
+    func generateMainLayout() -> NSCollectionLayoutSection {
+        let mainItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let mainItem = NSCollectionLayoutItem(layoutSize: mainItemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(2/3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [mainItem])
 
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 5
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                        leading: 5,
+                                                        bottom: 5,
+                                                        trailing: 5)
+        return section
+    }
+    
+    func generateListingLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1/3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                        leading: 5,
+                                                        bottom: 5,
+                                                        trailing: 2)
+        
+        return section
+    }
+    
+    
+}
+
+//https://www.raywenderlich.com/5436806-modern-collection-views-with-compositional-layouts
